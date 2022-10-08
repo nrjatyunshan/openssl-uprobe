9bea6bfe6c0145743c90039abceb7dd38ce0d3df
lanthora <lanthora@outlook.com>
2022-10-08 10:43:24 +0000

    格式化代码

--- openssl-uprobe/src/main.rs
+++ openssl-uprobe/src/main.rs
@@ -1,26 +1,24 @@
-use aya::{include_bytes_aligned, Bpf};
 use aya::programs::UProbe;
-use aya_log::BpfLogger;
+use aya::{include_bytes_aligned, Bpf};
 use clap::Parser;
-use log::{info, warn,LevelFilter};
+use log::{info, LevelFilter};
 use tokio::signal;
 
 #[derive(Debug, Parser)]
 struct Opt {
     #[clap(short, long)]
-    pid: Option<i32>
+    pid: Option<i32>,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     let opt = Opt::parse();
 
-    env_logger::builder().format_timestamp(None).filter_level(LevelFilter::Info).init();
+    env_logger::builder()
+        .format_timestamp(None)
+        .filter_level(LevelFilter::Info)
+        .init();
 
-    // This will include your eBPF object file as raw bytes at compile-time and load it at
-    // runtime. This approach is recommended for most real-world use cases. If you would
-    // like to specify the eBPF program at runtime rather than at compile-time, you can
-    // reach for `Bpf::load_file` instead.
     #[cfg(debug_assertions)]
     let mut bpf = Bpf::load(include_bytes_aligned!(
         "../../target/bpfel-unknown-none/debug/openssl-uprobe"
@@ -29,26 +27,27 @@ async fn main() -> Result<(), anyhow::Error> {
     let mut bpf = Bpf::load(include_bytes_aligned!(
         "../../target/bpfel-unknown-none/release/openssl-uprobe"
     ))?;
-    if let Err(e) = BpfLogger::init(&mut bpf) {
-        // This can happen if you remove all log statements from your eBPF program.
-        warn!("failed to initialize eBPF logger: {}", e);
-    }
 
-    let program: &mut UProbe = bpf.program_mut("uprobe_enter_openssl_write").unwrap().try_into()?;
+    let target = "libssl";
+    let fn_name = "uprobe_enter_openssl_write";
+    let program: &mut UProbe = bpf.program_mut(fn_name).unwrap().try_into()?;
     program.load()?;
-    program.attach(Some("SSL_write"), 0, "libssl", opt.pid.try_into()?)?;
+    program.attach(Some("SSL_write"), 0, target, opt.pid.try_into()?)?;
 
-    let program: &mut UProbe = bpf.program_mut("uprobe_exit_openssl_write").unwrap().try_into()?;
+    let fn_name = "uprobe_exit_openssl_write";
+    let program: &mut UProbe = bpf.program_mut(fn_name).unwrap().try_into()?;
     program.load()?;
-    program.attach(Some("SSL_write"), 0, "libssl", opt.pid.try_into()?)?;
+    program.attach(Some("SSL_write"), 0, target, opt.pid.try_into()?)?;
 
-    let program: &mut UProbe = bpf.program_mut("uprobe_enter_openssl_read").unwrap().try_into()?;
+    let fn_name = "uprobe_enter_openssl_read";
+    let program: &mut UProbe = bpf.program_mut(fn_name).unwrap().try_into()?;
     program.load()?;
-    program.attach(Some("SSL_read"), 0, "libssl", opt.pid.try_into()?)?;
+    program.attach(Some("SSL_read"), 0, target, opt.pid.try_into()?)?;
 
-    let program: &mut UProbe = bpf.program_mut("uprobe_exit_openssl_read").unwrap().try_into()?;
+    let fn_name = "uprobe_exit_openssl_read";
+    let program: &mut UProbe = bpf.program_mut(fn_name).unwrap().try_into()?;
     program.load()?;
-    program.attach(Some("SSL_read"), 0, "libssl", opt.pid.try_into()?)?;
+    program.attach(Some("SSL_read"), 0, target, opt.pid.try_into()?)?;
 
     info!("Waiting for Ctrl-C...");
     signal::ctrl_c().await?;
